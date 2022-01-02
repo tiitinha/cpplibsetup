e95c763e1af471f6c02b94186667cbc75efc816d
afroseppo <kobo-kobe@hotmail.com>
2022-01-02 14:44:15 +0200

    first commit

---
+++ libsetup/src/day19.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+#include "Strsplit.h"
+#include <iostream>
+#include <fstream>
+
+int main () {
+    return 0;
+}
+
---
+++ libsetup/strsplit/inc/Strsplit.h
@@ -0,0 +1,10 @@
+#ifndef STRSPLIT_H
+#define STRSPLIT_H
+
+#include <vector>
+#include <sstream>
+#include <string>
+#include <iterator>
+
+std::vector<std::string> split(const std::string &s, const std::string &delimiter);
+#endif
